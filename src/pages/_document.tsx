ae09a29f4ff76eceb2a631db160d0c403c0a9b28
Nestor <nestor.prado.tenorio@gmail.com>
2023-02-02 11:40:14 -0300

    first commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,12 @@
+import {Html,Head,Main, NextScript} from 'next/document';
+
+export default function Document(){
+    <Html>
+        <Head>
+        </Head>
+        <body>
+            <Main/>
+            <NextScript/>
+        </body>
+    </Html>
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Sujeito Pizza teste</h1>
+    </div>
+  )
+}
